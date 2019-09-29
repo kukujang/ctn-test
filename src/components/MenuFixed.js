14f43efb97f5657a60bbcaa6de3abf3e9ec89bdb
Nueng <kukujang@hotmail.com>
2019-09-30 01:36:54 +0700

    fix swiping

--- src/components/MenuFixed.js
+++ src/components/MenuFixed.js
@@ -5,6 +5,28 @@ import icon_8 from '../assets/img/sd.png';
 import {NavLink} from 'react-router-dom';
 
 export default class MenuFixed extends Component {
+  componentDidMount(){
+    window.addEventListener('touchstart', this.touchStart);
+    window.addEventListener('touchmove', this.preventTouch, {passive: false});
+  }
+  componentWillUnmount(){
+    window.removeEventListener('touchstart', this.touchStart);
+    window.removeEventListener('touchmove', this.preventTouch, {passive: false});
+  }
+  touchStart(e){
+    this.firstClientX = e.touches[0].clientX;
+    this.firstClientY = e.touches[0].clientY;
+  }
+  preventTouch(e){
+    const minValue = 5;
+    this.clientX = e.touches[0].clientX - this.firstClientX;
+    this.clientY = e.touches[0].clientY - this.firstClientY;
+    if(Math.abs(this.clientX) > minValue){ 
+        e.preventDefault();
+        e.returnValue = false;
+        return false;
+    }
+  }
   render() {
     return (
       <div className="bar-fixed">
